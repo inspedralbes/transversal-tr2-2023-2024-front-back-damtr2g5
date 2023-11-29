b74ea748d180b1308d528713396e02ecf9eaff6b
Joaguesan <a22joaguesan@inspedralbes.cat>
2023-11-29 22:31:47 +0100

    primeras pestañas aplicacion

--- vuetify-project/src/components/login/LoginForm.vue
+++ vuetify-project/src/components/login/LoginForm.vue
@@ -1,5 +1,6 @@
 <template>
-    <v-sheet class="d-flex align-center justify-center flex-wrap text-center mx-auto h-auto pa-4" elevation="4" rounded max-width="300" width="100%">
+    <v-sheet class="d-flex align-center justify-center flex-wrap text-center mx-auto h-auto pa-4" elevation="4" rounded
+        max-width="300" width="100%">
         <div>
             <div class="text-h5 font-weight-medium mb-2">
                 LOGIN
@@ -7,11 +8,32 @@
             <form>
                 <v-text-field v-model="username" label="First name"></v-text-field>
 
-                <v-text-field v-model="password" label="Password" ></v-text-field>
+                <v-text-field v-model="password" label="Password"></v-text-field>
 
                 <v-btn @click="guardar()" block class="mt-2">Login</v-btn>
             </form>
-           <p class="mt-3"> Aun no estas registrado? <v-btn @click="" variant="plain">Registrate</v-btn></p>
+            <p class="mt-3"> Aun no estas registrado? <a href="#" @click.stop.prevent="dialog = true"> Registrarse </a></p>
+            <v-dialog v-model="dialog" max-width="400" persistent>
+                <v-card>
+                    <v-card-title class="text-h5 bg-grey-lighten-3">
+                        Registrarse
+                    </v-card-title>
+                    <v-text-field v-model="username" label="Nombre" class="ma-4 mb-0"></v-text-field>
+                    <v-text-field v-model="surname" label="Apellidos" class="ma-4 mt-0 mb-0"></v-text-field>
+                    <v-text-field v-model="password" label="Contraseña" class="ma-4 mt-0 mb-0"></v-text-field>
+                    <v-text-field v-model="email" label="Email" class="ma-4 mb-0 mt-0"></v-text-field>
+                    <v-divider></v-divider>
+                    <v-card-actions>
+                        <v-btn variant="text" @click="dialog = false">
+                            Cancelar
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn color="deep-purple" variant="tonal" @click="dialog = false,guardar()">
+                            Aceptar
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </div>
     </v-sheet>
 </template>
@@ -28,14 +50,18 @@ export default {
     },
     data() {
         return {
+            dialog: false,
             username: '',
-            password: ''
+            password: '',
+            surname: '',
+            email: '',
         };
     },
     methods: {
-        guardar(){
+        guardar() {
             console.log(this.username, this.password);
-            this.appStore.setLoginInfo({name: this.username, contrasena: this.password,surname:'', email:'', rank:'', lifetotal:'', experience:'', image:''})
+            this.appStore.setLoginInfo({ name: this.username, contrasena: this.password, surname: this.surname, email: this.email, rank: '', lifetotal: '', experience: '', image: '' })
+            
         }
     }
 };
--- vuetify-project/src/router/index.js
+++ vuetify-project/src/router/index.js
@@ -4,20 +4,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: 'hola',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-      {path: '/pruebas',
-      component: () => import('@/components/login/LoginForm.vue')},
-    ],
-  },
+    component: () => import('@/views/Home.vue'),
+  }, {
+    path: '/pruebas',
+    component: () => import('@/views/PantallaLogin.vue')
+  }
 ]
 
 const router = createRouter({
