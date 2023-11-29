e9f369f5d8923865dd2ffa06d1fd76e6a41d8137
Joaguesan <a22joaguesan@inspedralbes.cat>
2023-11-29 10:53:45 +0100

    formato2 y formato4

--- vuetify-project/src/components/Formato2.vue
+++ vuetify-project/src/components/Formato2.vue
@@ -4,39 +4,45 @@
         <div>
             <div class="text-h5 font-weight-medium mb-2">
                 {{ preguntas.pregunta }}
+                <v-btn icon="mdi-cached" size="x-small" @click="reinicio"></v-btn>
             </div>
 
-            <v-row>
-                <v-col cols="6">
-                    <v-btn v-for="(respuestas, index) in preguntas.muestra" :key="index" class="respuesta-container">
-                        <v-btn v-for="(respuesta, idx) in respuestas" :key="idx"
-                            @click="toggleSeleccion(respuestas, respuesta)" outlined rounded
-                            :color="getButtonColor(respuesta)">
-                            {{ respuesta }}
-                        </v-btn>
-                    </v-btn>
-                </v-col>
-
-                <v-col cols="6">
-                    <v-btn @click="reinicio"  outlined rounded color="primary">
-                        Reinicio
-                    </v-btn>
-                </v-col>
-            </v-row>
-            {{ seleccionDer }}
-            {{ seleccionIzq }}
+            <v-btn-toggle :key="chartKey">
+                <v-btn v-for="(respuesta, index) in preguntas.muestra" :key="index" class="respuesta-container" outlined
+                    rounded @click="recoger(respuesta[0], 'cero')" :color="getButtonColor()"
+                    :disabled="isDisabled(respuesta[0])" :class="{ 'disable-input': disabled }">
+                    {{ respuesta[0] }}
+                </v-btn>
+            </v-btn-toggle>
+            <v-btn-toggle :key="chartKey1">
+                <v-btn v-for="(respuesta, index) in preguntas.muestra" :key="index" class="respuesta-container" outlined
+                    stacked rounded @click="recoger(respuesta[1], 'uno')" :color="getButtonColor()"
+                    :disabled="isDisabled(respuesta[1])" :class="{ 'disable-input': disabled }" >
+                    {{ respuesta[1] }}
+                </v-btn>
+            </v-btn-toggle>
         </div>
     </v-sheet>
 </template>
   
 <script>
+
+import { watch, ref } from 'vue'
 export default {
     data() {
+
+        const chartKey = ref(0)
+        const chartKey1 = ref(0)
+        const modelo = ref(0)
+        const updateChartData = () => {
+            chartKey.value += 1
+            chartKey1.value += 1
+        }
+
+        watch(() => modelo, () => {
+            updateChartData()
+        }, { deep: true })
         return {
-            respuestasSeleccionadas: {
-                primeraColumna: null,
-                segundaColumna: null
-            },
             preguntas: {
                 "_id": {
                     "$oid": "6565b1a56f122bd6cf8e9f0e"
@@ -83,44 +89,63 @@ export default {
                 "idTema": "4",
                 "formato": "Unir valores"
             },
-            seleccionIzq:[],            
-            seleccionDer:[],
-            colors:['red', 'green', 'blue', 'yellow'],
-            colors2:[],
+            seleccion: [],
+            colors: ['red', '#8ecae6', 'purple', 'yellow'],
+            indice1: 0,
+            indiceColors:0,
+            puesto1: false,
+            puesto2: false,
+            disabled: true,
+            modelo,
+            chartKey, chartKey1,
         };
     },
     created() {
-        this.seleccionIzq = this.preguntas.muestra.map(respuestas => respuestas[0]);
-        this.seleccionDer = this.preguntas.muestra.map(respuestas => respuestas[1]);
+        this.reinicio()
     },
     methods: {
-        toggleSeleccion(respuestas, respuesta) {
-            if (this.respuestasSeleccionadas.primeraColumna === null) {
-                this.respuestasSeleccionadas.primeraColumna = respuesta;
-            } else if (this.respuestasSeleccionadas.segundaColumna === null) {
-                this.respuestasSeleccionadas.segundaColumna = respuesta;
+        reinicio() {
+            this.seleccion = JSON.parse(JSON.stringify(this.preguntas.muestra));
+            for (let i = 0; i < this.preguntas.muestra.length; i++) {
+                for (let j = 0; j < this.preguntas.muestra[i].length; j++) {
+                    this.seleccion[i][j] = ''
+                }
+            }
+            this.modelo++
+        },
+        getButtonColor() {
+            return 'blue'
+        },
+        getDisabledColor() {
+            this.indiceColors++
+            return this.colors[this.indiceColors]
+        },
+        recoger(respuesta, pos) {
+            if (pos == 'cero') {
+                this.seleccion[this.indice1][0] = respuesta
+                this.puesto1 = true
             } else {
-                // Si ambos botones ya han sido seleccionados, reiniciar la selección
-                this.respuestasSeleccionadas = {
-                    primeraColumna: respuesta,
-                    segundaColumna: null
-                };
+                this.seleccion[this.indice1][1] = respuesta
+                this.puesto2 = true
+            }
+            if (this.puesto1 && this.puesto2) {
+                this.indice1++
+                this.puesto1 = false
+                this.puesto2 = false
+                if (this.indice1 == this.preguntas.muestra.length) {
+                    this.indice1 = 0
+                }
             }
         },
-        reinicio() {
-            this.respuestasSeleccionadas = {
-                primeraColumna: null,
-                segundaColumna: null
-            };
+        isDisabled(index) {
+            for (let i = 0; i < this.seleccion.length; i++) {
+                if (this.seleccion[i].includes(index)) {
+                    return true
+                }
+            }
         },
-        getButtonColor(respuesta) {
-            this.colors2.push(this.colors[1])
-            this.colors.splice(1, this.colors.length-1);
-
-            
-            return colors[this.preguntas.respuestas.findIndex(respuestas => respuestas.includes(respuesta)) % colors.length];
-        },
-    },
+    }
+    
 };
 </script>
   
@@ -130,6 +155,12 @@ export default {
     justify-content: space-between;
     margin-bottom: 5px;
     cursor: pointer;
+    flex-direction: column;
 }
+
+.disable-input:disabled {
+    background-color: v-bind(getDisabledColor()) !important;
+}
+
 </style>
   
