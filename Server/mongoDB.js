a91d6f063393e1bc1808dde3579ab19ec4513dea
YbarTom <121022918+osczapmar@users.noreply.github.com>
2023-11-30 09:09:57 +0100

    Mongo method to handle inserts and updates

--- Server/mongoDB.js
+++ Server/mongoDB.js
@@ -1,6 +1,6 @@
 const user = "a22osczapmar";
 const password = "Nitrome7.";
-module.exports = { getDocument, getPreguntas };
+module.exports = { getDocument, getPreguntas, getPregunta, insertInCollection, findRegisteredResult, updateCollection };
 const { MongoClient } = require("mongodb");
  
 // Replace the following with your Atlas connection string                                                                                                                                        
@@ -14,18 +14,61 @@ const client = new MongoClient(url);
 // Connect to the Atlas cluster
 client.connect();
 
-async function getDocument() {
+async function getDocument(id) {
     try {
         
          const db = client.db(dbName);
          const col = db.collection("activity");
          // Find and return the document
-         const filter = { "id": 1 };
+         const filter = { "id": id };
          const document = await col.findOne(filter);
          return document;
         } catch (err) {
          console.log(err.stack);
      }
+}
+
+async function findRegisteredResult(id_user, id_activity, id_pregunta) {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection("result");
+        const resultadoEncontrado = await col.findOne({},{idUser: id_user, idActivity: id_activity, idQuestion: id_pregunta})
+        if (resultadoEncontrado == null) {
+            return null
+        }
+        else {
+            return resultadoEncontrado
+        }
+    } catch(err) {
+        console.log(err.stack);
+    }
+}
+async function insertInCollection(data, collection){
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection(collection);
+        col.insertOne(data, function(err, res) {
+            if (err) throw err;
+            console.log("1 result inserted")
+        })
+    } catch(err) {
+        console.log(err.stack);
+    }
+}
+async function updateCollection(data, conditionals, collection) {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection(collection);
+        col.updateOne(conditionals, data, function(err, res) {
+            if (err) throw err;
+            console.log("1 result updated")
+        })
+    } catch(err) {
+        console.log(err.stack);
+    }
 }
 
 async function getPreguntas(preguntas) {
@@ -42,3 +85,18 @@ async function getPreguntas(preguntas) {
          console.log(err.stack);
      }
 }
+
+async function getPregunta(id) {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection("question");
+        //Find question from collection
+        const question = await col.find({"id":id}).toArray();
+        console.log(question)
+        return question;
+    } catch (err) {
+        console.log(err.stack)
+    }
+}
+
