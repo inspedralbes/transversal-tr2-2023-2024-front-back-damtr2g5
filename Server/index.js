dd45fca69f24467f4fd2a42022423ec4534bcea5
a22samgarhur <a22samgarhur@inspedralbes.cat>
2023-11-30 09:44:10 +0100

    Cambiado puerto del server de 3000 a 3001

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 3001;
 const { getDocument, getPreguntas, getPregunta, insertInCollection, findRegisteredResult, updateCollection } = require("./mongoDB.js");
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
