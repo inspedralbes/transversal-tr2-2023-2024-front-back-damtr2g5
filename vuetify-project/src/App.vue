c907cfdeb40037d80978da151a77733b37ace9b2
MarioMagia <a22marramgar@inspedralbes.cat>
2023-12-12 20:03:31 +0100

    estilo de login hecho

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -5,3 +5,29 @@
 <script setup>
   //
 </script>
+<style>
+@font-face {
+    font-family: 'monas';
+    src: url('@/assets/fonts/MonasRegular-7BZmR.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'short-baby';
+    src: url('@/assets/fonts/ShortBaby-Mg2w.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'confession';
+    src: url('@/assets/fonts/TheConfessionRegular.ttf') format('truetype');
+}
+@font-face {
+    font-family: 'confession-full';
+    src: url('@/assets/fonts/TheConfessionFullRegular.ttf') format('truetype');
+}
+@font-face {
+  font-family: 'tusj';
+  src: url('@/assets/fonts/FFF_Tusj.ttf') format('truetype');
+}
+@font-face {
+  font-family: 'acme';
+  src: url('@/assets/fonts/Acme-Regular.ttf') format('truetype');
+}
+</style>
--- vuetify-project/src/views/PantallaLogin.vue
+++ vuetify-project/src/views/PantallaLogin.vue
@@ -1,11 +1,14 @@
 <template>
-    <v-app class="maya-blue-bg">
-        <v-row style="width: fit-content; margin: auto; ">
-            <v-col cols="5" style="text-align: center;" align-self="center">
+    <div class="background-container">
+        <img src="@/assets/ringUFC4.jpg" alt="Background Image" class="background-image">
+    </div>
+    <v-app>
+        <v-row style="margin: auto;">
+            <v-col cols="12" xs="12" md="6" lg="6" sm="12" style="text-align: center;" align-self="center">
                 <img style="width: 100%; max-width: 1024px; height: auto;" src="@/assets/math_thai.png" alt="Logo"
                     class="logo">
             </v-col>
-            <v-col cols="7">
+            <v-col cols="12" md="6" lg="6" sm="12">
                 <v-container class="fill-height">
 
                     <LoginForm />
@@ -15,10 +18,25 @@
     </v-app>
 </template>
 
-<style></style>
+<style>
+.background-container {
+    position: fixed;
+    top: -1em;
+    left: -1em;
+    width: 103vw;
+    height: 103vh;
+    overflow: hidden;
+    filter: blur(10px);
+}
+
+.background-image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
   
 <script setup>
 
 import LoginForm from '@/components/login/LoginForm.vue'
 </script>
-  
