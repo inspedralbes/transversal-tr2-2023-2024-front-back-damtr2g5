21452ff5dd3a66bc43de145c2a87f170d57fcd6f
MarioMagia <a22marramgar@inspedralbes.cat>
2023-12-14 15:12:00 +0100

    otro ajuste a noticias

--- vuetify-project/src/components/login/Noticias.vue
+++ vuetify-project/src/components/login/Noticias.vue
@@ -1,8 +1,8 @@
 
 <template>
     <div class="bg myfont">
-        <v-row>
-            <v-col cols="12" sm="5" md="12" lg="12">
+        <v-row justify-sm="center" justify="center">
+            <v-col  cols="10" sm="6" md="12" lg="12">
                 <h2 class="bigger-font">Notícies</h2>
                 <div>
                     Sabías que para hacer que esto se vea medio bien he perdido 2 años de vida?
@@ -18,6 +18,7 @@
     background: url(..\..\assets\scroll-transparent.png);
     background-size: contain;
     background-repeat: no-repeat;
+    background-position: center;
     width: 100%;
     height: 31vmax;
     padding-top: 10%;
