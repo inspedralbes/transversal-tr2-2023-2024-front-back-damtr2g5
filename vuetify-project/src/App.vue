3e5c7361ee09458173be330b1b4a97cbb33cf09b
MarioMagia <a22marramgar@inspedralbes.cat>
2023-12-19 21:25:07 +0100

    Remove unused assets

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -7,26 +7,6 @@
 </script>
 <style>
 @font-face {
-    font-family: 'monas';
-    src: url('@/assets/fonts/MonasRegular-7BZmR.ttf') format('truetype');
-}
-@font-face {
-    font-family: 'short-baby';
-    src: url('@/assets/fonts/ShortBaby-Mg2w.ttf') format('truetype');
-}
-@font-face {
-    font-family: 'confession';
-    src: url('@/assets/fonts/TheConfessionRegular.ttf') format('truetype');
-}
-@font-face {
-    font-family: 'confession-full';
-    src: url('@/assets/fonts/TheConfessionFullRegular.ttf') format('truetype');
-}
-@font-face {
-  font-family: 'tusj';
-  src: url('@/assets/fonts/FFF_Tusj.ttf') format('truetype');
-}
-@font-face {
   font-family: 'acme';
   src: url('@/assets/fonts/Acme-Regular.ttf') format('truetype');
 }
