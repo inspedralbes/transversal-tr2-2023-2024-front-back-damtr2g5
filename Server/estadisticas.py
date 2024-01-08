e32f6c616160631ffd1f51a5b699534bb79adca8
Joaguesan <a22joaguesan@inspedralbes.cat>
2024-01-08 13:16:24 +0100

    inicio graficos

---
+++ Server/estadisticas.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import pandas as pd
+import json
+
+client = MongoClient("mongodb+srv://a22osczapmar:Nitrome7.@cluster0.uiii7nf.mongodb.net/")
+
+db = client['mathGameMongo']
+collection = db['history']
+data =  collection.find({"idUsuario": 41})
+datos={}
+'''
+for x in data:
+  datos["idUsuario"] = x["idUsuario"]
+  datos["idPregunta"] = x["idPregunta"]
+  datos["idEjercicio"] = x["idEjercicio"]
+  datos["respuesta"] = x["respuesta"]
+  datos["correcta"] = x["correcta"]
+'''
+for x in data:
+    datos["data"] = x
+
+df = pd.DataFrame(datos)
+
+print(df)
