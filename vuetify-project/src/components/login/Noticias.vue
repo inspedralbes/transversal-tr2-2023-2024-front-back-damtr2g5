63284a02f72894bbe0368627b293560dce61521e
a22samgarhur <a22samgarhur@inspedralbes.cat>
2024-01-09 12:11:22 +0100

    Cambiado noticias

--- vuetify-project/src/components/login/Noticias.vue
+++ vuetify-project/src/components/login/Noticias.vue
@@ -5,8 +5,8 @@
             <v-col  cols="10" sm="6" md="12" lg="12">
                 <h2 class="bigger-font">Notícies</h2>
                 <div>
-                    En aquest món hi ha 3 tipus de persones: les que saben comptar i les que no.
-                    <a href="https://forms.gle/3VPKF74xSnnbUVhc9">Pls contesten </a>
+                    En aquest món hi ha 3 tipus de persones: les que saben comptar i les que no.<br>
+                    <a href="https://forms.gle/3VPKF74xSnnbUVhc9">Valora la nostra web </a>
                 </div>
             </v-col>
         </v-row>
